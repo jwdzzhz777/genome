ddb53bef0f3c54ab084384cda8e86a5733715714
jwdzzhz777 <jwdzzhz777@gmail.com>
2019-11-14 15:31:09 +0800

    feat: 删除 d.ts 定义在 index.ts 中

--- index.d.ts
+++
@@ -1,8 +0,0 @@
-import 'egg';
-
-declare module 'egg' {
-    interface Controller {
-        /** 此方法只有 api 装饰器修饰过的 controller 才有！！ */
-        getMetadata(name: string): any;
-    }
-}
--- index.ts
+++ index.ts
@@ -1,2 +1,15 @@
-export * from './app/lib/decorators';
 export * from './app/lib/types';
+export * from './app/lib/decorators';
+
+/** 参考 egg-aop index.d.ts 依靠 ts 自动生成, egg declare 在入口处 */
+import 'egg';
+
+declare module 'egg' {
+    interface Controller {
+        /** 此方法只有 api 装饰器修饰过的 controller 才有！！ */
+        getMetadata(name: string): any;
+    }
+    interface Context {
+
+    }
+}
